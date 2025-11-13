a8bb14fff26f8c6023d94eb84b52631dfe5e5a1f
SavedByLight <mrfluffyoven@gmail.com>
2025-11-13 09:14:13 +0000

    cleanup initial tree

--- device.mk
+++ device.mk
@@ -6,3 +6,17 @@
 #
 
 LOCAL_PATH := device/samsung/a13
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
